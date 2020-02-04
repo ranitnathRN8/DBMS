25a0713eb0bb101aab2a253cff77f7b8275cb762
Ranit Nath <42543785+ranitnathRN8@users.noreply.github.com>
2020-02-04 16:40:13 +0530

    Update assignment3.sql

--- DBMS lab/Assignments/assignment3.sql
+++ DBMS lab/Assignments/assignment3.sql
@@ -6,3 +6,5 @@ select * from ORDERS where Odate = '1990-04-10';
 select * from CUSTOMERS where Snum = 1001 or Snum = 1004;
 
 select * from CUSTOMERS where Cname REGEXP '^[A-G].*$';
+
+select * from CUSTOMERS where Cname REGEXP '^C.*$';
