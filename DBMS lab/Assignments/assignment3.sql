0f863e6c98ec2ca7e7e4006abcebad79a52275aa
Ranit Nath <42543785+ranitnathRN8@users.noreply.github.com>
2020-02-18 15:39:07 +0530

    Update assignment3.sql

--- DBMS lab/Assignments/assignment3.sql
+++ DBMS lab/Assignments/assignment3.sql
@@ -1,9 +1,8 @@
 use asg1;
 
-select * from ORDERS where Odate = '1990-03-10';
-select * from ORDERS where Odate = '1990-04-10';
+select * from ORDERS where Odate in (select Odate from ORDERS where Odate = '1990-03-10' or Odate = '1990-04-10');
 
-select * from CUSTOMERS where Snum = 1001 or Snum = 1004;
+select * from CUSTOMERS where Snum in (select Snum from SALESPERSON where Sname = 'Peel' or Sname = 'Motika');
 
 select * from CUSTOMERS where Cname REGEXP '^[A-G].*$';
 
